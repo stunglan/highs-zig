b77ad4b4046efe4bccee0b19306fd96359565c58
Knut Sebastian Tungland <stunglan@gmail.com>
2024-10-02 05:46:00 +0200

    add lpcall

---
+++ src/highs.zig
@@ -0,0 +1,136 @@
+const std = @import("std");
+
+const chighs = @cImport({
+    @cInclude("highs_c_api.h");
+});
+const testing = std.testing;
+
+pub const HighsInt = chighs.HighsInt;
+pub const HighsUInt = c_uint;
+
+pub const kHighsMaximumStringLength = chighs.kHighsMaximumStringLength;
+
+pub const kHighsStatusError = chighs.kHighsStatusError;
+pub const kHighsStatusOk = chighs.kHighsStatusOk;
+pub const kHighsStatusWarning = chighs.kHighsStatusWarning;
+
+pub const kHighsVarTypeContinuous = chighs.kHighsVarTypeContinuous;
+pub const kHighsVarTypeInteger = chighs.kHighsVarTypeInteger;
+pub const kHighsVarTypeSemiContinuous = chighs.kHighsVarTypeSemiContinuous;
+pub const kHighsVarTypeSemiInteger = chighs.kHighsVarTypeSemiInteger;
+pub const kHighsVarTypeImplicitInteger = chighs.kHighsVarTypeImplicitInteger;
+
+pub const kHighsOptionTypeBool = chighs.kHighsOptionTypeBool;
+pub const kHighsOptionTypeInt = chighs.kHighsOptionTypeInt;
+pub const kHighsOptionTypeDouble = chighs.kHighsOptionTypeDouble;
+pub const kHighsOptionTypeString = chighs.kHighsOptionTypeString;
+
+pub const kHighsInfoTypeInt64 = chighs.kHighsInfoTypeInt64;
+pub const kHighsInfoTypeInt = chighs.kHighsInfoTypeInt;
+pub const kHighsInfoTypeDouble = chighs.kHighsInfoTypeDouble;
+pub const kHighsObjSenseMinimize = chighs.kHighsObjSenseMinimize;
+pub const kHighsObjSenseMaximize = chighs.kHighsObjSenseMaximize;
+
+pub const kHighsMatrixFormatColwise = chighs.kHighsMatrixFormatColwise;
+pub const kHighsMatrixFormatRowwise = chighs.kHighsMatrixFormatRowwise;
+
+pub const kHighsHessianFormatTriangular = chighs.kHighsHessianFormatTriangular;
+pub const kHighsHessianFormatSquare = chighs.kHighsHessianFormatSquare;
+
+pub const kHighsSolutionStatusNone = chighs.kHighsSolutionStatusNone;
+pub const kHighsSolutionStatusInfeasible = chighs.kHighsSolutionStatusInfeasible;
+pub const kHighsSolutionStatusFeasible = chighs.kHighsSolutionStatusFeasible;
+
+pub const kHighsBasisValidityInvalid = chighs.kHighsBasisValidityInvalid;
+pub const kHighsBasisValidityValid = chighs.kHighsBasisValidityValid;
+
+pub const kHighsPresolveStatusNotPresolved = chighs.kHighsPresolveStatusNotPresolved;
+pub const kHighsPresolveStatusNotReduced = chighs.kHighsPresolveStatusNotReduced;
+pub const kHighsPresolveStatusInfeasible = chighs.kHighsPresolveStatusInfeasible;
+pub const kHighsPresolveStatusUnboundedOrInfeasible = chighs.kHighsPresolveStatusUnboundedOrInfeasible;
+pub const kHighsPresolveStatusReduced = chighs.kHighsPresolveStatusReduced;
+pub const kHighsPresolveStatusReducedToEmpty = chighs.kHighsPresolveStatusReducedToEmpty;
+pub const kHighsPresolveStatusTimeout = chighs.kHighsPresolveStatusTimeout;
+pub const kHighsPresolveStatusNullError = chighs.kHighsPresolveStatusNullError;
+pub const kHighsPresolveStatusOptionsError = chighs.kHighsPresolveStatusOptionsError;
+pub const kHighsPresolveStatusOutOfMemory = chighs.kHighsPresolveStatusOutOfMemory;
+
+pub const kHighsModelStatusNotset = chighs.kHighsModelStatusNotset;
+pub const kHighsModelStatusLoadError = chighs.kHighsModelStatusLoadError;
+pub const kHighsModelStatusModelError = chighs.kHighsModelStatusModelError;
+pub const kHighsModelStatusPresolveError = chighs.kHighsModelStatusPresolveError;
+pub const kHighsModelStatusSolveError = chighs.kHighsModelStatusSolveError;
+pub const kHighsModelStatusPostsolveError = chighs.kHighsModelStatusPostsolveError;
+pub const kHighsModelStatusModelEmpty = chighs.kHighsModelStatusModelEmpty;
+pub const kHighsModelStatusOptimal = chighs.kHighsModelStatusOptimal;
+pub const kHighsModelStatusInfeasible = chighs.kHighsModelStatusInfeasible;
+pub const kHighsModelStatusUnboundedOrInfeasible = chighs.kHighsModelStatusUnboundedOrInfeasible;
+pub const kHighsModelStatusUnbounded = chighs.kHighsModelStatusUnbounded;
+pub const kHighsModelStatusObjectiveBound = chighs.kHighsModelStatusObjectiveBound;
+pub const kHighsModelStatusObjectiveTarget = chighs.kHighsModelStatusObjectiveTarget;
+pub const kHighsModelStatusTimeLimit = chighs.kHighsModelStatusTimeLimit;
+pub const kHighsModelStatusIterationLimit = chighs.kHighsModelStatusIterationLimit;
+pub const kHighsModelStatusUnknown = chighs.kHighsModelStatusUnknown;
+pub const kHighsModelStatusSolutionLimit = chighs.kHighsModelStatusSolutionLimit;
+pub const kHighsModelStatusInterrupt = chighs.kHighsModelStatusInterrupt;
+
+pub const kHighsBasisStatusLower = chighs.kHighsBasisStatusLower;
+pub const kHighsBasisStatusBasic = chighs.kHighsBasisStatusBasic;
+pub const kHighsBasisStatusUpper = chighs.kHighsBasisStatusUpper;
+pub const kHighsBasisStatusZero = chighs.kHighsBasisStatusZero;
+pub const kHighsBasisStatusNonbasic = chighs.kHighsBasisStatusNonbasic;
+
+pub const kHighsCallbackLogging = chighs.kHighsCallbackLogging;
+pub const kHighsCallbackSimplexInterrupt = chighs.kHighsCallbackSimplexInterrupt;
+pub const kHighsCallbackIpmInterrupt = chighs.kHighsCallbackIpmInterrupt;
+pub const kHighsCallbackMipSolution = chighs.kHighsCallbackMipSolution;
+pub const kHighsCallbackMipImprovingSolution = chighs.kHighsCallbackMipImprovingSolution;
+pub const kHighsCallbackMipLogging = chighs.kHighsCallbackMipLogging;
+pub const kHighsCallbackMipInterrupt = chighs.kHighsCallbackMipInterrupt;
+pub const kHighsCallbackMipGetCutPool = chighs.kHighsCallbackMipGetCutPool;
+pub const kHighsCallbackMipDefineLazyConstraints = chighs.kHighsCallbackMipDefineLazyConstraints;
+
+pub const kHighsCallbackDataOutLogTypeName = chighs.kHighsCallbackDataOutLogTypeName;
+pub const kHighsCallbackDataOutRunningTimeName = chighs.kHighsCallbackDataOutRunningTimeName;
+pub const kHighsCallbackDataOutSimplexIterationCountName = chighs.kHighsCallbackDataOutSimplexIterationCountName;
+pub const kHighsCallbackDataOutIpmIterationCountName = chighs.kHighsCallbackDataOutIpmIterationCountName;
+pub const kHighsCallbackDataOutPdlpIterationCountName = chighs.kHighsCallbackDataOutPdlpIterationCountName;
+pub const kHighsCallbackDataOutObjectiveFunctionValueName = chighs.kHighsCallbackDataOutObjectiveFunctionValueName;
+pub const kHighsCallbackDataOutMipNodeCountName = chighs.kHighsCallbackDataOutMipNodeCountName;
+pub const kHighsCallbackDataOutMipPrimalBoundName = chighs.kHighsCallbackDataOutMipPrimalBoundName;
+pub const kHighsCallbackDataOutMipDualBoundName = chighs.kHighsCallbackDataOutMipDualBoundName;
+pub const kHighsCallbackDataOutMipGapName = chighs.kHighsCallbackDataOutMipGapName;
+pub const kHighsCallbackDataOutMipSolutionName = chighs.kHighsCallbackDataOutMipSolutionName;
+pub const kHighsCallbackDataOutCutpoolNumColName = chighs.kHighsCallbackDataOutCutpoolNumColName;
+pub const kHighsCallbackDataOutCutpoolNumCutName = chighs.kHighsCallbackDataOutCutpoolNumCutName;
+pub const kHighsCallbackDataOutCutpoolNumNzName = chighs.kHighsCallbackDataOutCutpoolNumNzName;
+pub const kHighsCallbackDataOutCutpoolStartName = chighs.kHighsCallbackDataOutCutpoolStartName;
+pub const kHighsCallbackDataOutCutpoolIndexName = chighs.kHighsCallbackDataOutCutpoolIndexName;
+pub const kHighsCallbackDataOutCutpoolValueName = chighs.kHighsCallbackDataOutCutpoolValueName;
+pub const kHighsCallbackDataOutCutpoolLowerName = chighs.kHighsCallbackDataOutCutpoolLowerName;
+pub const kHighsCallbackDataOutCutpoolUpperName = chighs.kHighsCallbackDataOutCutpoolUpperName;
+
+pub const lpCall_result = struct {
+    col_value: []f64,
+    col_dual: []f64,
+    row_value: []f64,
+    row_dual: []f64,
+    col_basis_status: []i32,
+    row_basis_status: []i32,
+    model_status: i32,
+    run_status: i32,
+};
+
+//pub extern fn Highs_lpCall(num_col: HighsInt, num_row: HighsInt, num_nz: HighsInt, a_format: HighsInt, sense: HighsInt, offset: f64, col_cost: [*c]const f64, col_lower: [*c]const f64, col_upper: [*c]const f64, row_lower: [*c]const f64, row_upper: [*c]const f64, a_start: [*c]const HighsInt, a_index: [*c]const HighsInt, a_value: [*c]const f64, col_value: [*c]f64, col_dual: [*c]f64, row_value: [*c]f64, row_dual: [*c]f64, col_basis_status: [*c]HighsInt, row_basis_status: [*c]HighsInt, model_status: [*c]HighsInt) HighsInt;
+
+pub fn Highs_lpCall_zig(num_col: i32, num_row: HighsInt, num_nz: HighsInt, a_format: HighsInt, sense: HighsInt, offset: f64, col_cost: []f64, col_lower: []f64, col_upper: []f64, row_lower: []f64, row_upper: []f64, a_start: []HighsInt, a_index: []HighsInt, a_value: []f64, col_value: []f64, col_dual: []f64, row_value: []f64, row_dual: []f64, col_basis_status: []HighsInt, row_basis_status: []HighsInt, model_status: *HighsInt) i32 {
+    return chighs.Highs_lpCall(num_col, num_row, num_nz, a_format, sense, offset, col_cost.ptr, col_lower.ptr, col_upper.ptr, row_lower.ptr, row_upper.ptr, a_start.ptr, a_index.ptr, a_value.ptr, col_value.ptr, col_dual.ptr, row_value.ptr, row_dual.ptr, col_basis_status.ptr, row_basis_status.ptr, model_status);
+}
+
+pub export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
+}
---
+++ src/main.zig
@@ -0,0 +1,94 @@
+const std = @import("std");
+const expect = @import("std").testing.expect;
+const assert = @import("std").debug.assert;
+const highs = @import("highs.zig");
+
+// Mimic the functionality in call_highs_from_c_minimal
+fn minimal_api() void {
+
+    // The HighsInt type is either int or long, depending on the HiGHS build
+    const num_col = 2;
+    const num_row = 3;
+    const num_nz = 5;
+
+    // Define the optimization sense and objective offset
+    //var sense = highs.kHighsObjSenseMinimize;
+    var sense = highs.kHighsObjSenseMinimize;
+    const offset: f64 = 3;
+
+    // Define the column costs, lower bounds and upper bounds
+    var col_cost = [2]f64{ 1, 1 };
+    var col_lower = [2]f64{ 0, 1 };
+    var col_upper = [2]f64{ 4.0, 1.0e30 };
+
+    // Define the row lower bounds and upper bounds
+    var row_lower = [3]f64{ -1.0e30, 5.0, 6.0 };
+    var row_upper = [3]f64{ 7.0, 15.0, 1.0e30 };
+
+    // Define the constraint matrix column-wise
+    const a_format = highs.kHighsMatrixFormatColwise;
+    var a_start = [2]i32{ 0, 2 };
+    var a_index = [5]i32{ 1, 2, 0, 1, 2 };
+    var a_value = [5]f64{ 1.0, 3.0, 1.0, 2.0, 2.0 };
+
+    var objective_value: f64 = 0;
+    var col_value = [_]f64{0.0} ** num_col;
+    var col_dual = [_]f64{0.0} ** num_col;
+    var row_value = [_]f64{0.0} ** num_row;
+    var row_dual = [_]f64{0.0} ** num_row;
+
+    var col_basis_status = [_]i32{0.0} ** num_col;
+    var row_basis_status = [_]i32{0.0} ** num_row;
+
+    var model_status: i32 = 0;
+    var run_status: i32 = 0;
+
+    run_status =
+        highs.Highs_lpCall_zig(num_col, num_row, num_nz, a_format, sense, offset, &col_cost, &col_lower, &col_upper, &row_lower, &row_upper, &a_start, &a_index, &a_value, &col_value, &col_dual, &row_value, &row_dual, &col_basis_status, &row_basis_status, &model_status);
+
+    if (run_status != highs.kHighsStatusOk) {
+        std.log.err("Error in run_status {}", .{run_status});
+    }
+    if (model_status != highs.kHighsModelStatusOptimal) {
+        std.log.err("Error in run_status {}", .{model_status});
+    }
+
+    objective_value = offset;
+
+    std.log.info("\nRun status = {d}; Model status = {d}", .{ run_status, model_status });
+
+    objective_value = offset;
+
+    // Report the column primal and dual values, and basis status
+    for (0.., col_value, col_dual, col_basis_status, col_cost) |i, value, dual, basis_status, cost| {
+        std.log.info("Col%{d} = {d}; dual = {d}; status = {d}", .{ i, value, dual, basis_status });
+        objective_value += value * cost;
+    }
+
+    // Report the column primal and dual values, and basis status
+    for (0.., row_value, row_dual, row_basis_status) |i, value, dual, basis_status| {
+        std.log.info("Row%{d} = {d}; dual = {d}; status = {d}", .{ i, value, dual, basis_status });
+    }
+
+    std.log.info("Optimal objective value = {}", .{objective_value});
+
+    sense = highs.kHighsObjSenseMaximize;
+
+    return;
+}
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.", .{"loving"});
+
+    std.debug.print("{}", .{highs.add(1, 2)});
+
+    minimal_api();
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
